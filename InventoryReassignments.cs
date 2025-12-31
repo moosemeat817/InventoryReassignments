13632eb32af8dd8dc960c66715a4a7d69c22b91e
moosemeat817 <michaelsd123456@gmail.com>
2025-12-31 11:04:18 -0600

    Add files via upload

--- InventoryReassignments.cs
+++ InventoryReassignments.cs
@@ -1,15 +1,14 @@
 using MelonLoader;
 using MelonLoader.Utils;
-using ComplexLogger;
 
 namespace InventoryReassignments
 {
     public class Main : MelonMod
     {
-        internal static ComplexLogger<Main> Logger = new();
+
         public override void OnInitializeMelon()
         {
-            Logger.Log("InventoryReassignments is online.", FlaggedLoggingLevel.None);
+            MelonLogger.Msg("InventoryReassignments is online.");
             Settings.OnLoad();    // ModSettings
         }
 
